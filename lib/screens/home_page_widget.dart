ca934e2b76721ea6c8d9e79ab11f30ca7a0743d5
Rafael <rafaelbalko@gmail.com>
2026-05-04 18:09:05 -0300

    Modificação na home page

--- lib/screens/home_page_widget.dart
+++ lib/screens/home_page_widget.dart
@@ -1,6 +1,8 @@
 import 'package:aula1_hello_flutter/screens/login_page_widget.dart';
-import 'package:flutter/material.dart';
+import 'package:aula1_hello_flutter/screens/tela1_widget.dart';
+import 'package:aula1_hello_flutter/screens/tela2_widget.dart';
 import 'package:aula1_hello_flutter/services/auth_service.dart';
+import 'package:flutter/material.dart';
 
 class HomePageWidget extends StatefulWidget {
   const HomePageWidget({super.key});
@@ -28,8 +30,8 @@ class _HomePageWidgetState extends State<HomePageWidget> {
       barrierDismissible: false,
       builder: (context) {
         return AlertDialog(
-          title: Text('Atenção!'),
-          content: Text('Deseja realmente sair?'),
+          title: Text('Atenção'),
+          content: Text('Deseja sair da aplicação?'),
           actions: [
             TextButton(
               onPressed: () => Navigator.of(context).pop(),
@@ -54,6 +56,31 @@ class _HomePageWidgetState extends State<HomePageWidget> {
     );
   }
 
+  void _showAboutDialog(BuildContext context) {
+    showAboutDialog(
+      context: context,
+      applicationIcon: Icon(Icons.cast_for_education),
+      applicationName: 'Aula 2 - Navegação',
+      applicationVersion: '1.0.0',
+      applicationLegalese: 'Todos os diretos reservados',
+      children: [
+        SizedBox(height: 10),
+        Text(
+          'ADS FEMA 2026',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        Row(
+          children: [
+            Icon(Icons.check_box_outlined, color: Colors.green),
+            SizedBox(width: 4),
+            Text('Uso do Navigator'),
+          ],
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,16 +88,14 @@ class _HomePageWidgetState extends State<HomePageWidget> {
         title: Text('Home Page'),
         actions: [
           IconButton(
-            onPressed: () {
-              _showLogoutDialog(context);
-            },
+            onPressed: () => _showLogoutDialog(context),
             icon: Icon(Icons.logout_outlined),
           ),
         ],
       ),
       drawer: Drawer(
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
+          borderRadius: BorderRadiusGeometry.only(
             topRight: Radius.circular(20),
             bottomRight: Radius.circular(20),
           ),
@@ -86,21 +111,89 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                       gradient: LinearGradient(
                         begin: Alignment.topLeft,
                         end: Alignment.bottomRight,
-                        colors: [
-                          Colors.blue.shade700,
-                          Colors.blue.shade400,
-                        ],
+                        colors: [Colors.blue.shade700, Colors.blue.shade400],
                       ),
                     ),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.end,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('Rafael Balko'),
-                        Text('rafaelbalko@gmail.com'),
+                        CircleAvatar(
+                          radius: 25,
+                          backgroundColor: Colors.white,
+                          child: Icon(Icons.person, color: Colors.blue),
+                        ),
+                        SizedBox(height: 12),
+                        Text(
+                          'Guilherme de Cleva Farto',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 4),
+                        Text('guilherme.farto@gmail.com'),
                       ],
                     ),
                   ),
+                  ListTile(
+                    leading: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade50,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Icon(Icons.looks_one, color: Colors.blue.shade700),
+                    ),
+                    title: Text('Tela 1'),
+                    trailing: Icon(
+                      Icons.arrow_forward,
+                      color: Colors.grey.shade600,
+                    ),
+                    onTap: () {
+                      // encerrar (fechar) o menu lateral
+                      Navigator.of(context).pop();
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Tela1Widget()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    leading: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade50,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Icon(Icons.looks_two, color: Colors.blue.shade700),
+                    ),
+                    title: Text('Tela 2'),
+                    trailing: Icon(
+                      Icons.arrow_forward,
+                      color: Colors.grey.shade600,
+                    ),
+                    onTap: () {
+                      // encerrar (fechar) o menu lateral
+                      Navigator.of(context).pop();
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Tela2Widget()),
+                      );
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  ListTile(
+                    leading: Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Icon(Icons.info, color: Colors.white),
+                    ),
+                    title: Text('Sobre'),
+                    onTap: () {
+                      Navigator.of(context).pop();
+                      _showAboutDialog(context);
+                    },
+                  ),
+                  SizedBox(height: 10),
                   ListTile(
                     leading: Container(
                       padding: EdgeInsets.all(8),
@@ -108,10 +201,59 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                         color: Colors.red.shade900,
                         borderRadius: BorderRadius.circular(8),
                       ),
-                      child: Icon(Icons.logout_outlined, color: Colors.white),
+                      child: Icon(Icons.logout, color: Colors.white),
                     ),
-                    title: Text('Sair'),
+                    title: Text('Sair...'),
                     onTap: () => _showLogoutDialog(context),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                border: Border(
+                  top: BorderSide(color: Colors.grey.shade600, width: 1.5),
+                ),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.info_outlined,
+                        size: 14,
+                        color: Colors.grey.shade400,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        'Versão 1.0.0',
+                        style: TextStyle(
+                          color: Colors.grey.shade400,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 4,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.blue.shade200,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Text(
+                      'Aula 2',
+                      style: TextStyle(
+                        color: Colors.blue.shade400,
+                        fontSize: 12,
+                      ),
+                    ),
                   ),
                 ],
               ),
@@ -120,12 +262,8 @@ class _HomePageWidgetState extends State<HomePageWidget> {
         ),
       ),
       body: Center(
-        child: Text(
-          'Bem-vindo à Home!',
-          style: TextStyle(fontSize: 28),
-        ),
+        child: Text('Bem-vindo à Home!', style: TextStyle(fontSize: 28)),
       ),
     );
   }
 }
-
